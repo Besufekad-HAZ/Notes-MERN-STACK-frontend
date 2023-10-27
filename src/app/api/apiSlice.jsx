9ba5e1c45c949e7564dbb4fc413ed79795ffc17c
Besufekad-HAZ <besurye@gmail.com>
2023-10-27 16:38:29 +0300

    Adjust fetch base query in the apislice.js file

--- src/app/api/apiSlice.jsx
+++ src/app/api/apiSlice.jsx
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://bemni-tech-repair.onrender.com",
+  baseUrl: "https://bemni-tech-repair-api.onrender.com",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
